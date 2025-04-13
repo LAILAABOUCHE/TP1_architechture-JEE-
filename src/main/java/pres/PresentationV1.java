0c5e86a68b1b7e471d474ae5fb67c289601ffeab
LAILAABOUCHE <lailaabouche02@gmail.com>
2025-04-13 13:01:06 +0100

    updates

--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -1,10 +1,17 @@
 package pres;
+import DAO.daoIMPL;
 import  metier.metierIMP;
+
+import static metier.metier.dao;
+
 public class PresentationV1 {
 
      public static void main(String[]args){
 
+         daoIMPL d= new daoIMPL();
          metierIMP metier= new metierIMP();
+         metier.setDAO(d);
+         System.out.println("RES=" +metier.calcul());
      }
 
 
